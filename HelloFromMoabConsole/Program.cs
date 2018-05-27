170870940ce97e6767f7289eee8ef212adedd442
Karl Archuleta <karlsarch@outlook.com>
2018-05-26 21:45:42 -0600

    Initial check-in

--- HelloFromMoabConsole/Program.cs
+++ HelloFromMoabConsole/Program.cs
@@ -10,6 +10,13 @@ namespace HelloFromMoabConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Moab!");
+            Console.WriteLine("Hello Cam. Watch out for Cactus.");
+            Console.WriteLine("Hello Carson. Remember to wait for us at intersections.");
+            Console.WriteLine("Hello Kevin. ...Kevin? Where's Kevin?");
+
+            Console.WriteLine("\r\nPress any key to exit.");
+            Console.ReadKey();
         }
     }
 }
