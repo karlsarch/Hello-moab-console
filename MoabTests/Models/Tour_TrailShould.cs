26400115da6a32cfba035684c89e3969408eaeb7
Carson Archuleta <autoson@comcast.net>
2018-07-14 19:04:40 -0600

    Wrote my version of the IsValid function

--- MoabTests/Models/Tour_TrailShould.cs
+++ MoabTests/Models/Tour_TrailShould.cs
@@ -12,7 +12,7 @@ namespace MoabTests.Models
     public class Tour_TrailShoud
     {
         [Theory]
-        [InlineData(0, 1000, 0, true)]
+        [InlineData(0, 1000, 0, true)] 
         [InlineData(0, 1000, 1, true)]
         [InlineData(0, 1000, 999, true)]
         [InlineData(0, 1000, 1000, true)]
@@ -22,7 +22,7 @@ namespace MoabTests.Models
         [InlineData(10, 900, 901, false)]
         [InlineData(null, 1000, 9, true)]
         [InlineData(10, null, 1000, true)]
-        [InlineData(null, null, null, false)]
+        [InlineData(null, null, null, true)]
         [InlineData(50, 50, 50, true)]
         [InlineData(50, 50, null, true)]
         [InlineData(null, 990, 1, true)]
