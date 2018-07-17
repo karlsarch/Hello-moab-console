a311a1a40b322c17626cc2d662467aae059f8516
Karl Archuleta <karlsarch@outlook.com>
2018-07-17 13:25:18 -0600

    Added collection for testing to ExerciseImportHelperShould class.

--- MoabTests/Models.Helpers/ExerciseImportHelperShould.cs
+++ MoabTests/Models.Helpers/ExerciseImportHelperShould.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Nymbl.Models.POCO;
 
 namespace MoabTests.Models.Helpers
 {
     class ExerciseImportHelperShould
     {
+        public ICollection<Exercise> _existingExercises = new HashSet<Exercise>();
+
+        /// <summary>
+        /// Constructor.  Populates the existingExercise collection for use by test methods
+        /// </summary>
+        public ExerciseImportHelperShould()
+        {
+            var ex1 = new Exercise
+            {
+                ExerciseCode = "FLX_003_L",
+                Name = "Old Calf stretch Left",
+                EasierHint = "Easy, Partner",
+                HarderHint = "As a diamond."
+            };
+
+            var ex2 = new Exercise
+            {
+                ExerciseCode = "FLX_003_R",
+                Name = "Old Calf stretch Right",
+                EasierHint = "Easy, Partner",
+                HarderHint = "As a diamond."
+            };
+
+            var ex3 = new Exercise
+            {
+                ExerciseCode = "STAB_012_X",
+                Name = "Standing weight shift",
+                EasierHint = "Don't move",
+                HarderHint = "Close your eyes"
+            };
+
+            var ex4 = new Exercise
+            {
+                ExerciseCode = "KSA_999_X",
+                Name = "One Finger pull-up",
+                EasierHint = "Use two fingers",
+                HarderHint = "Pinky Finger"
+            };
+
+            _existingExercises.Add(ex1);
+            _existingExercises.Add(ex2);
+            _existingExercises.Add(ex3);
+            _existingExercises.Add(ex4);
+        }
+
+        // Add Unit Tests here...
     }
 }
